9b4113db188bdde3f1d193112c06eaccfb13de1a
minimalgeek <minimalgeek@gmail.com>
2016-01-31 06:28:21 +0100

    load levels

--- TheGame/Assets/Script/LevelLoader.cs
+++ TheGame/Assets/Script/LevelLoader.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
 
 public class LevelLoader : MonoBehaviour
 {
 
     public void LoadNextLevel()
     {
-        string sceneName = "Level" + GameState.instance.LevelState.currentLevel + 1;
-        if (SceneManager.GetSceneByName(sceneName).IsValid())
-        {
-            GameState.instance.LevelState.currentLevel++;
-            SceneManager.LoadScene(sceneName);
-        } else
+        string sceneName = "Level" + (GameState.instance.LevelState.currentLevel + 1);
+        GameState.instance.LevelState.currentLevel++;
+        try {
+            Application.LoadLevel(sceneName);
+        } catch (Exception e)
         {
             GameState.instance.LevelState.currentLevel = 1;
-            LoadCurrentLevel();
         }
     }
 
     public void LoadCurrentLevel()
     {
-        SceneManager.LoadScene("Level" + GameState.instance.LevelState.currentLevel);
+        string sceneName = "Level" + (GameState.instance.LevelState.currentLevel);
+        try
+        {
+            Application.LoadLevel(sceneName);
+        }
+        catch (Exception e)
+        {
+            GameState.instance.LevelState.currentLevel = 1;
+        }
     }
 
 }

3d187b7e3d668e225e0e934e93d5ab5b733a8418
minimalgeek <minimalgeek@gmail.com>
2016-01-31 14:42:03 +0100

    Panels

--- TheGame/Assets/Script/Controller/RitualColliderController.cs
+++ TheGame/Assets/Script/Controller/RitualColliderController.cs
@@ -11,11 +11,15 @@ public class RitualColliderController : MonoBehaviour {
     private CultistController cultistController;
     private SoundController soundController;
 
+    public GameObject pentaMonsta;
+
     void Start()
     {
         cultistController = FindObjectOfType<CultistController>();
         levelAndClockController = FindObjectOfType<LevelAndClockController>();
         soundController = FindObjectOfType<SoundController>();
+        pentaMonsta = GameObject.Find("PentaMonsta");
+        pentaMonsta.GetComponent<SpriteRenderer>().sortingOrder = -10;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -41,7 +45,19 @@ public class RitualColliderController : MonoBehaviour {
             string gameObjectName = this.gameObject.name;
             GameState.instance.LevelState.AddExecutedRitual(gameObjectName);
             Destroy(image.GetComponent<Image>());
-            Destroy(gameObject);
+
+            if (gameObjectName == "Pentagram")
+            {
+                pentaMonsta.GetComponent<SpriteRenderer>().sortingOrder = 0;
+                pentaMonsta.transform.position = gameObject.transform.position;
+                Destroy(gameObject);
+            } else if (gameObjectName == "Shower" || gameObjectName == "Poop")
+            {
+                // TODO figure it out
+            } else 
+            {
+                Destroy(gameObject);
+            }
 
             levelAndClockController.ProgessAndCheckWin(gameObjectName);
             cultistController.Animate(gameObjectName);
